6f00f8bfdf364ab8f22333a170d771ea8d9063c7
min <mirgwak@yahoo.com>
2017-06-21 17:31:39 -0700

    062117:master:rfserver -add listen timeout

--- src/rfclient.py
+++ src/rfclient.py
@@ -8,24 +8,73 @@ Created on Jun 20, 2017
 import sys
 from bluetooth import *
 
+class myClient(object):
+    '''For Client '''
+    def __init__(self,a_name,a_addr=None):
+        #self.first_match
+        self.port = None
+        self.name = None
+        self.host = None
+        self.client_sock = None
+        self.is_found = "No"
+        
+        self.myscan(a_name,a_addr)
+        #self.myconnect()
+        #self.mysend("Sending message from client")
+        #self.myreceive()
+        #self.mydisconnect()
+        
+    def __str__(self):
+        ret = self.is_found
+        return ret       
+        
+    def myscan(self,_name,_addr):  
+        print("Scan device...")
+        device_match = find_service(name=_name,address=_addr )
 
-service_match = find_service(name="sloanled")
-
-if len(service_match) == 0:
-    print("cannot find the server.")
-    sys.exit(0)
+        if len(device_match) == 0:
+            print("cannot find the server.")
+            self.is_found = "No"
+            sys.exit(0)
+        else:
+            print ("found {} matches".format(len(device_match)) )
+            self.is_found = "Yes"
     
-first_match = service_match[0]
-port = first_match["port"]
-name = first_match["name"]
-host = first_match["host"]
+        first_match = device_match[0]
+        self.port = first_match["port"]
+        self.name = first_match["name"]
+        self.host = first_match["host"]
+        print("connect port:{},name:{},host:{}".format(self.port,self.name,self.host) )
 
-print("connect to ", host)
+    def myconnect(self):
+        print("connecting...")
+        self.client_sock=BluetoothSocket(RFCOMM)
+        self.client_sock.connect((self.host,self.port))
+        print('Okay')
+        
+    def mysend(self,message):   
+        self.client_sock.send(message)
+        print("sent message.")
+        
+    def myreceive(self):    
+        data = self.client_sock.recv(80)
+        print ("received:", data)
 
-sock=BluetoothSocket(RFCOMM)
-sock.connect((host,port))
-sock.send("pyBluz Client sent")
-data = sock.recv(80)
+    def mydisconnect(self):
+        print("disconnect.")
+        self.client_sock.close()
 
-print ("received:", data)
-sock.close()
+
+if __name__ == '__main__':
+    myname = "sloanled"
+    client_addr = "00:1A:7D:DA:71:08"
+    server_addr = "B8:27:EB:97:2B:42"
+    
+    c = myClient("sloanled")
+    print(c)
+    c.myconnect()
+    c.mysend("Sent message from client.")
+    c.myreceive()
+    c.mydisconnect()
+
+
--- src/rfserver.py
+++ src/rfserver.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 '''
+rfserver.py
 Created on Jun 20, 2017
 @author: min
 '''
@@ -7,19 +8,54 @@ Created on Jun 20, 2017
 import sys
 from bluetooth import *
 
-server_sock=BluetoothSocket(RFCOMM)
-server_sock.bind(("",PORT_ANY))
-localaddr,localport = server_sock.getsockname()
-server_sock.listen(1)
 
-advertise_service(server_sock,name="sloanled",service_classes=[SERIAL_PORT_CLASS],profiles=[SERIAL_PORT_PROFILE])
+class myServer(object):
+    
+    def __init__(self):
+        self.client_sock = None
+        #self.client_info = None
+        self.server_sock = BluetoothSocket(RFCOMM)
+        self.server_sock.settimeout(5.0)
+        self.server_sock.bind( ("",PORT_ANY) )
+        self.server_sock.listen(1)
+         
+        
+    def getserverinfo(self):    
+        server_addr,server_port = self.server_sock.getsockname() 
+        print ("server addr={}, port={}".format(server_addr,server_port))
+ 
+        
+    def myadvertising(self,a_name):  
+        print("start advertising...")    
+        advertise_service(self.server_sock,name=a_name,service_classes=[SERIAL_PORT_CLASS],profiles=[SERIAL_PORT_PROFILE])
 
-client_sock,client_info = server_sock.accept()
-print("accepted connection form", client_info)
+        self.client_sock,client_info = self.server_sock.accept()
+        print("accepted connection form", client_info)
 
-client_sock.send("pyBluez server sent")
-data = client_sock.recv[1024]
-print("received: ",data)
+    def mysend(self,message):
+        self.client_sock.send(message)
+        print("sent message")
+        
+    def myreceive(self):    
+        data = self.client_sock.recv[1024]
+        print("received: ",data)
 
-client_sock.close()
-server_sock.close()
+    def disconnect(self):
+        self.client_sock.close()
+        self.server_sock.close()
+
+if __name__ == '__main__':
+    myname = "sloanled"
+    client_addr = "00:1A:7D:DA:71:08"
+    server_addr = "B8:27:EB:97:2B:42"
+    
+    s=myServer()
+    s.getserverinfo()
+    s.myadvertising("sloanled")
+    s.mysend("sent message from server.")
+    s.myreceive()
+    s.disconnect()
+
+
+
+
